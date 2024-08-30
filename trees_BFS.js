0b23c352c887992d05773a1c9527dfcab042abd3
Duc Do <doduyminhduc@gmail.com>
2024-08-30 20:20:10 +0700

    traverse trees

---
+++ trees_BFS.js
@@ -0,0 +1,102 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this. right = null;
+    }
+}
+
+class BST {
+    constructor(){
+        this.root = null;
+    }
+
+    // This is insert the whole Node which is including: value, left pointer, right pointer
+    // input para is just a value
+    insert(value) {
+        let newNode = new Node(value);
+        //if the BST is null
+        if (!this.root) {
+            //temp points to new node
+            this.root = newNode;
+            return this;
+        }
+
+        let temp = this.root;
+        //Use temp to move to the appropirate node then compare value
+        //if the BST is not null
+        while(true) {
+            if (newNode.value === temp.value) return undefined; // if value of newNode equal root.value
+            if (newNode.value < temp.value) {
+                if (temp.left === null){ // if the left pointer is null
+                    temp.left = newNode;
+                    return this;
+                }
+                temp = temp.left; // keep iterate throught the left side on the BST
+            }
+            if (newNode.value > temp.value) {
+                if(temp.right === null) { // if the right pointer is null 
+                    temp.right = newNode;
+                    return this;
+                } 
+                temp = temp.right;
+            }
+        }
+    }
+    contains(value) { // here is actually we only take the value and compare with the Node.value in the existing BST
+        // If the BST is null
+        if (!this.root) return false;
+        // If the BST is not null
+        //Use temp to move to the appropirate node then compare value
+        let temp = this.root;
+        while(temp){
+            if(value === temp.value) return true;
+            if (value < temp.value) {
+                temp = temp.left;
+            }else if (value > temp.value){
+                temp = temp.right;
+            }
+
+        }
+        return false;
+    }
+
+    BFS(){
+        // Use currentNode to hold the root node
+        let currentNode = this.root;
+
+        // use queue array to temporary hold the Node the you pop out from the tree
+        let queue = [];
+
+        // use result array to hold value from the node
+        let results = [];
+
+        //keep the currentNode in queue
+        queue.push(currentNode);
+
+        while(queue.length) {
+            //1. pop out the current node in queue
+            currentNode = queue.shift();
+
+            //2. Keep value of the currentNode in resultes array
+            results.push(currentNode.value);
+
+            //3. if the left & right node exist, add to the queue
+            if(currentNode.left) queue.push(currentNode.left);
+            if(currentNode.right) queue.push(currentNode.right);
+        }
+        return results;
+    }
+
+}
+
+let myTree = new BST();
+myTree.insert(47);
+myTree.insert(21);
+myTree.insert(76);
+myTree.insert(18);
+myTree.insert(27);
+myTree.insert(52);
+myTree.insert(82);
+
+
---
+++ trees_DFS_Postorder.js
@@ -0,0 +1,124 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+ 
+class BST {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+        const newNode = new Node(value);
+        if (this.root === null) {
+            this.root = newNode;
+            return this;
+        }
+        let temp = this.root;
+        while (true) {
+            if (newNode.value === temp.value) return undefined;
+            if (newNode.value < temp.value) {
+                if (temp.left === null) {
+                    temp.left = newNode;
+                    return this;
+                }
+                temp = temp.left;
+            } else {
+                if (temp.right === null) {
+                    temp.right = newNode;
+                    return this;
+                } 
+                temp = temp.right;
+            }
+        }
+    }
+
+    contains(value) {
+        if (this.root === null) return false;
+        let temp = this.root;
+        while (temp) {
+            if (value < temp.value) {
+                temp = temp.left;
+            } else if (value > temp.value) {
+                temp = temp.right;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }      
+    
+    minValueNode(currentNode) {
+        while (currentNode.left !== null) {
+            currentNode = currentNode.left;
+        }
+        return currentNode;
+    }
+
+    //BFS utilise a Queue and Array to iterate throught the tree
+    // Not use recursion
+    BFS() {
+        let currentNode = this.root;
+        let results = [];
+        let queue = [];
+        queue.push(currentNode);
+
+        while (queue.length) {
+           currentNode = queue.shift();
+           results.push(currentNode.value);
+           if(currentNode.left) queue.push(currentNode.left);
+           if(currentNode.right) queue.push(currentNode.right);
+        }
+        return results;
+    }
+
+    // Use recursion and write the first visited node into the array
+    DFSPostOrder(){
+        //itiative an array to keep array result
+        let results = [];
+
+        //1. Recursion to go throught the tree
+
+        function traverse(currentNode){
+
+            //2. if there is the left node, visit the left node; go all the way left
+            if (currentNode.left) traverse(currentNode.left);
+
+            //3. if there is the right node, visit the right node
+            if (currentNode.right) traverse(currentNode.right);
+
+            //4. push the node to the array
+            results.push(currentNode.value); // this is the base case
+        }
+        traverse(this.root);
+        return results;
+    }
+
+    
+}
+ 
+ 
+
+let myTree = new BST();
+
+myTree.insert(47);
+myTree.insert(21);
+myTree.insert(76);
+myTree.insert(18);
+myTree.insert(27);
+myTree.insert(52);
+myTree.insert(82);
+
+console.log( myTree.DFSPostOrder() );
+
+
+
+/*
+    EXPECTED OUTPUT:
+    ----------------
+    [ 18, 27, 21, 52, 82, 76, 47 ]
+
+*/  
